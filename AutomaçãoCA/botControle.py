49875818f2648f5ed01e0de0af48cab404cfb398
jorgegazzatto <71462700+jorgegazzatto@users.noreply.github.com>
2024-01-09 21:15:24 +0000

    Inicio

---
+++ AutomaçãoCA/botControle.py
@@ -0,0 +1,6 @@
+# [Google chrome] - abrir o navegador
+#[Painel Acionamento] digitar - https://gstihml.intranet/Incidente/Acionamento
+#[Login/senha+OTP no Smartit] - https://smartit.local.srv.br/smartit/app/#/
+
+
+import pandas as pd
